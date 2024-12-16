ca93d46a48a2a519cd4f23192c4357b73602cf28
swomf <swomf@proton.me>
2024-12-16 08:31:42 -0500

    config: user can pick wanted wlan name via ncurses
    As a replacement for the older install.sh, `make config`
    can be used to select the most pertinent network interface
    name based on the output of ip.c.
    
    In a nutshell:
    * User runs `make config`
    * ip.c is run
    * An ncurses menu appears based on the interfaces listed
    * User selects a network interface name
    * A sed file is created
    * Now, when netmask.zsh-theme is compiled, the selected network
      interface name is what is searched for.
    
    Remember that the default specified in netmask.zsh-theme.tmpl searches
    for interfaces that start with "wl", which are the most common.
    
    New targets:
    * config (as mentioned)
    * distclean (cleans up sed files)

--- Makefile
+++ Makefile
@@ -1,22 +1,30 @@
 SRC_DIR     := src
+CONFIG_DIR  := $(SRC_DIR)/config
+CONFIG_PROG := $(CONFIG_DIR)/configure
 IP_PROG     := $(SRC_DIR)/ip
 THEME       := $(SRC_DIR)/netmask.zsh-theme
 
 all: $(IP_PROG) $(THEME)
 
 help:
-	@echo "Usage: make [all|clean|install]"
+	@echo "Usage: make [all|config|clean|distclean|install]"
 	@echo ""
 	@echo "Targets: "
 	@echo "  all       - all targets"
+	@echo "  config    - configure netmask.zsh-theme for user's specific IP"
+	@echo "                (default: find first ip that starts with `wl`)"
 	@echo "  clean     - remove compiled targets"
-	@echo "  install   - install netmask.zsh-theme and ip binaries t"
+	@echo "  distclean - clean, also remove 'make config' output"
+	@echo "  install   - install netmask.zsh-theme and ip binaries to"
 	@echo "              ZSH/custom"
 	@echo ""
-	@echo "Recommended usage: make all && make install"
+	@echo "Recommended usage: make config && make all && make install"
 
 clean:
-	rm -f $(IP_PROG) $(THEME)
+	rm -f $(IP_PROG) $(THEME) $(CONFIG_PROG)
+
+distclean: clean
+	rm -f $(wildcard $(CONFIG_DIR)/*.sed)
 
 install: all
 ifndef ZSH
@@ -24,10 +32,18 @@ ifndef ZSH
 	using `sudo -E make install`. Otherwise, install ohmyzsh and retry)
 endif
 	install -Dm644 $(THEME) $(ZSH)/custom/themes/netmask.zsh-theme
+	install -Dm755 $(IP_PROG) $(ZSH)/custom/bin/netmask/ip
+
+config: $(CONFIG_PROG) $(IP_PROG)
+	./$(CONFIG_PROG)
+
+$(CONFIG_PROG): $(CONFIG_PROG).c
+	$(CC) $(CFLAGS) $(LDFLAGS) $$(pkg-config --libs ncurses menu) -o $@ $^
 
 $(IP_PROG): $(IP_PROG).c
 
-$(THEME): $(THEME).tmpl
+$(THEME): $(THEME).tmpl $(wildcard $(CONFIG_DIR)/*.sed)
 	cp -f $(THEME).tmpl $(THEME)
+	$(foreach file, $(wildcard $(CONFIG_DIR)/*.sed), sed -f $(file) -i $(THEME);)
 
-.PHONY: all clean help install
+.PHONY: all config clean distclean help install
---
+++ src/config/configure.c
@@ -0,0 +1,167 @@
+#include <menu.h>
+#include <ncurses.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Technically NI_MAXHOST is 1025 (see <netdb.h>), so the maximum
+// buffer size should be approx. 1025+3+19. But nobody's _THAT_ crazy.
+#define BUFFER_SIZE 32
+#define MAX_LINES 10 /* arbitrary number */
+
+int get_ip_choices(char **argv, char lines[MAX_LINES][BUFFER_SIZE],
+                   unsigned int *total_lines);
+int main_loop(char lines[MAX_LINES][BUFFER_SIZE], unsigned int total_lines);
+int create_sed(char **argv, char *line);
+
+void handle_sigint(int sig) {
+    endwin(); // Restore terminal settings
+    printf("Caught signal %d. Exiting gracefully...", sig);
+    exit(0); // Exit the program
+}
+
+int main(int argc, char **argv) {
+  signal(SIGINT, handle_sigint);
+  
+  if (argc != 1) {
+    fprintf(stderr, "Only provide the executable path.\n");
+    return 1;
+  }
+
+  // Get IP choices -- data for ncurses options
+  char lines[MAX_LINES][BUFFER_SIZE];
+  unsigned int total_lines = 0;
+  if (get_ip_choices(argv, lines, &total_lines) != 0) {
+    fprintf(stderr, "Failed to get IP choices.\n");
+    return 1;
+  }
+
+  // Run ncurses
+  int line_index = main_loop(lines, total_lines);
+
+  // Create actual sed script from first word of selected line
+  create_sed(argv, lines[line_index]);
+
+  return 0;
+}
+
+int get_ip_choices(char **argv, char lines[MAX_LINES][BUFFER_SIZE],
+                   unsigned int *total_lines) {
+  // Get real path of src/ip binary
+  char *path = realpath(argv[0], NULL);
+  if (path == NULL) {
+    perror("Failed to get real path");
+    return 1;
+  }
+
+  for (int i = strlen(path) - 1, slashes_found = 0; i >= 0; i--) {
+    if (path[i] == '/') {
+      slashes_found++;
+      if (slashes_found == 2) {
+        strcpy(&path[i], "/ip");
+        break;
+      }
+    }
+  }
+
+  // Read IP choices from binary stdout
+  FILE *pipe = popen(path, "r");
+  if (pipe == NULL) {
+    perror("Failed to open pipe");
+    free(path);
+    return 1;
+  }
+
+  while (*total_lines < MAX_LINES &&
+         fgets(lines[*total_lines], BUFFER_SIZE, pipe) != NULL) {
+    lines[*total_lines][strcspn(lines[*total_lines], "\n")] =
+        0; /* Remove newline */
+    (*total_lines)++;
+  }
+
+  pclose(pipe);
+  free(path); // Free the path after use
+  return 0;
+}
+
+int main_loop(char lines[MAX_LINES][BUFFER_SIZE], unsigned int total_lines) {
+  int ch, highlight = 0;
+
+  initscr();
+  noecho();
+  curs_set(0);
+  keypad(stdscr, TRUE);
+  clear();
+
+  addstr("CONFIGURATION\n");
+  addstr("=============\n");
+  addstr("Select the IPv4 that you want to see in your shell.\n");
+  while (1) {
+    refresh();
+    for (int i = 0; i < total_lines; i++) {
+      if (i == highlight) {
+        attron(A_REVERSE);
+        mvaddstr(i+3, 2, lines[i]);
+        attroff(A_REVERSE);
+      } else {
+        mvaddstr(i+3, 2, lines[i]);
+      }
+      addstr("\n");
+    }
+    ch = getch();
+    if (ch == KEY_UP && highlight > 0) {
+      highlight--;
+    } else if (ch == KEY_DOWN && highlight < total_lines - 1) {
+      highlight++;
+    } else if (ch == '\n') {
+      break;
+    }
+  }
+  endwin();
+
+  return highlight;
+}
+
+int create_sed(char **argv, char *line) {
+  // Build real path of src/config/wireless.sed
+  char *path = realpath(argv[0], NULL);
+  if (path == NULL) {
+    perror("Failed to get real path");
+    return 1;
+  }
+
+  for (int i = strlen(path) - 1, slashes_found = 0; i >= 0; i--) {
+    if (path[i] == '/') {
+      slashes_found++;
+      if (slashes_found == 1) {
+        path[i] = '\0';
+        strcat(path, "/wireless.sed");
+        break;
+      }
+    }
+  }
+
+  // Extract actual WiFi interface
+  char *interface = strtok(line, " ");
+  if (interface == NULL) {
+    perror("Failed to get interface");
+    free(path);
+    return 1;
+  }
+
+  // Create sed script
+  FILE *file = fopen(path, "w");
+  if (file == NULL) {
+    perror("Failed to open file");
+    free(path);
+    return 1;
+  }
+
+  fprintf(file, "# Patches for netmask.zsh-theme\n");
+  fprintf(file, "s:ip wl:ip %s:g\n", interface);
+
+  fclose(file);
+  free(path);
+  return 0;
+}
